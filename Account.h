3cbadaa6a0233789d4645f5dbeb60378589d0989
PatrickBladeeman <zhiyuan.zhao@utexas.edu>
2023-11-28 17:48:57 -0600

    headers change

---
+++ Account.h
@@ -0,0 +1,27 @@
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+
+class Account {
+protected:
+    double balance;
+    int numDeposits;
+    int numWithdrawals;
+    double annualInterestRate;
+    double monthlyServiceCharges;
+
+public:
+    Account(double initialBalance, double interestRate);
+
+    virtual void deposit(double amount);
+    virtual void withdraw(double amount);
+    virtual void calcInt();
+    virtual void monthlyProc();
+
+    double getBalance() const;
+    int getNumDeposits() const;
+    int getNumWithdrawals() const;
+    double getAnnualInterestRate() const;
+    double getMonthlyServiceCharges() const;
+};
+
+#endif // ACCOUNT_H
