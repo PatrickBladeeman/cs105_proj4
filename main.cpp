5cce6587b9b7728feeff358329c3b24f1365f714
Heyu Zhou <reginazh@cs.utexas.edu>
2023-12-01 20:45:03 -0600

    pretty much finished with displaying in main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <stdio.h>
+#include <iomanip>  
 #include <iostream>
 #include <string>
 #include <cmath>
@@ -49,19 +50,20 @@ int main() {
             curAccount->deposit(amount);
             cout << "deposit: $" << amount << endl;
         } else if (amount < 0) {
-            curAccount->withdraw(amount);
-            cout << "withdraw: -$" << abs(amount) << endl;
+            curAccount->withdraw(abs(amount));
+            cout << "withdraw: $" << abs(amount) << endl;
         }
     }
 //     beginning balance, total dollar amount
 // of deposits, total dollar amount of withdrawals, interest dollar amount, service
 // charges dollar amount, and ending balance.
-    cout << "Beginning Balance: " << initialBalance << endl;
-    cout << "Total deposits: " << curAccount->getDeposits() << endl;
-    cout << "Total withdrawals: " << curAccount->getWithdrawals() << endl;
-    cout << "Interest dollar amount: " << curAccount->getAnnualInterestRate() << endl;
-    cout << "Total service charges: " << curAccount->getWithdrawals() << endl;
-    cout << "Ending balance: " << curAccount->getWithdrawals() << endl;
+    cout << "Monthly Summary: " << endl;
+    cout << "Beginning Balance: $" << setprecision(3) << initialBalance << endl;
+    cout << "Total deposits: $" << setprecision(3) << curAccount->getDeposits() << endl;
+    cout << "Total withdrawals: $" << setprecision(3) << curAccount->getWithdrawals() << endl;
+    cout << "Monthly Interest: $" << curAccount->getAnnualInterestRate() / 12.0<< endl;
+    cout << "Total service charges: $" << setprecision(3) << curAccount->getMonthlyServiceCharges() << endl;
+    cout << "Ending balance: $" << setprecision(3) << curAccount->getBalance()<< endl;
     curAccount->monthlyProc();
     return 0;
 }
